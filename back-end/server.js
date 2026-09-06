b52b768f8634179cef09659f87c7adf3d3c33a87
Adilov Timur <adilovtimur14@gmail.com>
2026-09-06 15:32:50 +0500

    test for docker

--- back-end/server.js
+++ back-end/server.js
@@ -1,3 +1,4 @@
+import fsSync from 'fs';
 import express from 'express';
 import fs from 'fs/promises';
 import cors from 'cors';
@@ -9,7 +10,13 @@ const PORT = 3001;
 app.use(cors());
 app.use(express.json());
 
-const FILE_PATH = path.resolve('text.txt');
+const DATA_DIR = path.resolve('data');
+
+if (!fsSync.existsSync(DATA_DIR)) {
+  fsSync.mkdirSync(DATA_DIR);
+}
+
+const FILE_PATH = path.resolve('data', 'text.txt');
 
 app.post('/save', async (req, res) => {
   const { text } = req.body;
@@ -27,4 +34,5 @@ app.post('/save', async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`🚀 Сервер запущен на http://localhost:${PORT}`);
+  console.log(`📁 Данные сохраняются в: ${FILE_PATH}`);
 });
