4305b95da883c07965b86f21758e07c72916b35f
Adilov Timur <adilovtimur14@gmail.com>
2026-09-22 14:28:56 +0500

    feat(recursion-task): задачи по рекурсии

--- front-end/src/algo/recursion/recursion.ts
+++ front-end/src/algo/recursion/recursion.ts
@@ -9,3 +9,33 @@ function sumArray(arr: number[]): number {
 }
 
 console.log(sumArray([1, 2, 3, 4]));
+
+function findMax(arr: number[]): number {
+  let max = arr[0];
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  const restMax = findMax(arr.slice(1));
+  if (max < restMax) {
+    max = restMax;
+    return max;
+  } else {
+    return max;
+  }
+}
+
+console.log(findMax([101, 10, 120, 1, 2, 100, 3, 4, 5, 12, 102]));
+
+function power(base: number, exp: number): number {
+  if (exp === 0) {
+    return 1;
+  }
+
+  if (exp < 0) {
+    return 1 / power(base, -exp);
+  }
+
+  return base * power(base, exp - 1);
+}
+
+console.log(power(2, -2));
