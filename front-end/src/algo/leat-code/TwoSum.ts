fa15a728ea3c7bb453399874a23c02568c65defb
Adilov Timur <adilovtimur14@gmail.com>
2026-09-24 15:11:05 +0500

    feat(leat-code): Тут решение, так же создал файл для экспортов, в будущем не буду упоменть когда он будет изменяться

---
+++ front-end/src/algo/leat-code/TwoSum.ts
@@ -0,0 +1,18 @@
+const arrayInt: number[] = [2, 7, 11, 15];
+export function twoSum(nums: number[], target: number): number[] {
+  const seen = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    const findInt = target - nums[i];
+    const foundIndex = seen.get(findInt);
+    console.log(foundIndex, findInt);
+
+    if (foundIndex !== undefined) {
+      return [foundIndex, i];
+    }
+    seen.set(nums[i], i);
+  }
+  return [];
+}
+
+console.log(twoSum(arrayInt, 17));
