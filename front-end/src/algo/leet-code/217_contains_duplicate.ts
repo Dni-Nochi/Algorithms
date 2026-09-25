eace2a82111c9fbd44fd0de8205d1dee1acc593c
Adilov Timur <adilovtimur14@gmail.com>
2026-09-25 11:52:09 +0500

    feat(leet-code)/217_contains_duplicate: Решил задачу Contains Duplicate

---
+++ front-end/src/algo/leet-code/217_contains_duplicate.ts
@@ -0,0 +1,12 @@
+export function containsDupLicate(nums: number[]): boolean {
+  const seen = new Set<number>();
+  for (let i = 0; i < nums.length; i++) {
+    if (seen.has(nums[i])) {
+      return true;
+    }
+    seen.add(nums[i]);
+  }
+  return false;
+}
+
+// console.log(containsDupLicate([1, 2, 3, 4]));
